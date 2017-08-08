feabcc370413277b656fd98f615fcf1112d806d9
af <a.fabiano@live.fr>
2017-08-08 16:29:50 +0200

    wikinimous DONE

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -8,17 +8,40 @@ class ArticlesController < ApplicationController
   end
 
   def new
+    @article = Article.new
   end
 
   def create
+    @article = Article.new(article_params)
+    if @article.save
+      redirect_to articles_path
+    else
+      render :new
+    end
   end
 
   def edit
+    @article = Article.find(params[:id])
   end
 
   def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      redirect_to articles_path
+    else
+      render :edit
+    end
   end
 
   def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    redirect_to articles_path
+  end
+
+  private
+
+  def article_params
+    params.require(:article).permit(:title, :content)
   end
 end
